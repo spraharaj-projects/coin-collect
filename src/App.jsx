1a2b8b2bb648d9a02086368d847eb1996715ad9a
Supreet Praharaj <supreet.praharaj.pro+github@gmail.com>
2023-07-30 16:50:16 +0530

    Added Login

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,30 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Game from './components/game/Game'
 import Admin from './components/admin/Admin'
+import LoginForm from './components/loginForm/LoginForm'
+import { useState } from 'react'
 
 function App() {
-  
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  // const placeCoin = () => {
+  //   const { x, y } = getRandomSafeSpot()
+  //   const coinRef = ref(database, `coins/${getKeyString(x, y)}`)
+  //   set(coinRef, {
+  //     x,
+  //     y,
+  //   })
+  //   console.log('coins', coins)
+  //   const coinTimeOuts = [2000, 3000, 4000, 5000]
+  //   setTimeout(() => {
+  //     placeCoin()
+  //   }, randomFromArray(coinTimeOuts))
+  // }
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Game />} />
+        <Route path="/" element={<LoginForm onLogin={setIsLoggedIn} />} />
+        <Route path="/game" element={<Game />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
